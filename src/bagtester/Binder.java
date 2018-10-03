a398fdf0ba6aa0225048f0d12a53d5571b6e8342
unknown <Jakub.Borsuk@student.tdsb.on.ca>
2018-10-03 13:38:30 -0400

    finished binder class
    added all methods and variables

--- src/bagtester/Binder.java
+++ src/bagtester/Binder.java
@@ -10,5 +10,83 @@ package bagtester;
  * @author 342720539
  */
 public class Binder {
+
+    private String label;
+    private String colour;
+    private int numSheets;
+
+    final int emptyBinder = 200; //in grams
+    final double paperWeight = 4.5; //in grams
+    double binderWeight; //in grams
     
+    public Binder(String lbl, String clr, int sheets){
+        this.label = lbl;
+        this.colour = clr;
+        this.numSheets = sheets;
+    }
+    
+    public void addSheets(int amt){
+        this.setNumSheets(this.getNumSheets() + amt);
+    }
+    
+    public double totalWeight(){
+        binderWeight = emptyBinder + (this.getNumSheets()*paperWeight);
+        
+        return binderWeight;
+    }
+    
+    public Binder compareWeight(Binder b){
+        if(this.totalWeight()>= b.totalWeight()){
+            return this;
+        }else{
+            return b;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return "Label: "+this.getLabel()+", Colour "+this.getColour()+", Sheets: "+this.getNumSheets();
+    }
+
+    /**
+     * @return the label
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * @param label the label to set
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    /**
+     * @return the colour
+     */
+    public String getColour() {
+        return colour;
+    }
+
+    /**
+     * @param colour the colour to set
+     */
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    /**
+     * @return the numSheets
+     */
+    public int getNumSheets() {
+        return numSheets;
+    }
+
+    /**
+     * @param numSheets the numSheets to set
+     */
+    public void setNumSheets(int numSheets) {
+        this.numSheets = numSheets;
+    }
 }
--- src/bagtester/SchoolBag.java
+++ src/bagtester/SchoolBag.java
@@ -10,5 +10,15 @@ package bagtester;
  * @author 342720539
  */
 public class SchoolBag {
+
+    private String style;
+    private Binder [] binder = new Binder [2];
     
+    final double emptyBag = 1000; //in grams
+    
+    public SchoolBag(String s, Binder b1, Binder b2){
+        this.style = s;
+        this.binder[0] = b1;
+        this.binder[1] = b2;
+    }
 }
